3a8c9f68cd7333652e1e2752971603b51a0a0efb
EulerLCarvalho <127429284+EulerLCarvalho@users.noreply.github.com>
2023-10-01 18:14:24 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,44 @@ void main() {
   runApp(const MyApp());
 }
 
+class Pessoa {
+  double altura;
+  double peso;
+
+  Pessoa({required this.altura, required this.peso});
+
+  double calcularIMC() {
+    return peso / (altura * altura);
+  }
+}
+
+class ListaDeImc {
+  List<String> lista = [];
+
+  void adicionarIMC(String imc) {
+    lista.add(imc);
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculadora de IMC',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 11, 139, 212)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Calculadora de IMC'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +50,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController alturaController = TextEditingController();
+  TextEditingController pesoController = TextEditingController();
+  String resultado = '';
+  ListaDeImc listaIMC = ListaDeImc(); // Adicionando uma instância de ListaDeImc
 
-  void _incrementCounter() {
+  void calcularIMC() {
+    double altura = double.parse(alturaController.text);
+    double peso = double.parse(pesoController.text);
+
+    Pessoa pessoa = Pessoa(altura: altura, peso: peso);
+    double imc = pessoa.calcularIMC();
+
+    listaIMC.adicionarIMC(imc.toStringAsFixed(2)); // Adicionando o IMC à lista
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      resultado = 'Seu IMC é: ${imc.toStringAsFixed(2)}';
     });
+  }
+
+  void salvarResultado() {
+    double altura = double.parse(alturaController.text);
+    double peso = double.parse(pesoController.text);
+
+    Pessoa pessoa = Pessoa(altura: altura, peso: peso);
+    double imc = pessoa.calcularIMC();
+
+    listaIMC.adicionarIMC(imc.toStringAsFixed(2));
+
+    setState(() {
+      resultado = 'Seu IMC é: ${imc.toStringAsFixed(2)}';
+    });
+  }
+
+  void mostraResultadosSalvos() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Resultados Salvos'),
+          content: Column(
+            children: listaIMC.lista.map((resultado) {
+              return Text('Seu Imc é $resultado');
+            }).toList(),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Fechar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    EdgeInsetsGeometry padding = EdgeInsets.all(20.0);
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: padding,
+              child: TextField(
+                  controller: alturaController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Altura (m)',
+                  )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: padding,
+              child: TextField(
+                  controller: pesoController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Peso (kg)',
+                  )),
             ),
+            ElevatedButton(
+              onPressed: calcularIMC,
+              child: Text('Calcular IMC'),
+            ),
+            ElevatedButton(
+              onPressed: salvarResultado,
+              child: Text('Salvar Resultado'),
+            ),
+            ElevatedButton(
+                onPressed: mostraResultadosSalvos,
+                child: Text('Mostrar Resultados Salvos')),
+            SizedBox(height: 20),
+            Text(resultado),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
